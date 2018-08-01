b2b80884960ee5fb2db273c43fab481b53e2bebd
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2018-08-01 23:51:12 +0900

    Add upload function

--- get_drive_data.py
+++ get_drive_data.py
@@ -1,10 +1,11 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
 from apiclient.discovery import build
-from apiclient.http import MediaIoBaseDownload
+from apiclient.http import MediaIoBaseDownload, MediaFileUpload
 import pickle
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument('mode', type=str)
 parser.add_argument('file_name', type=str)
 parser.add_argument('output_file', type=str)
 args = parser.parse_args()
@@ -25,26 +26,31 @@ if not credentials:
 with open("credentials.pkl", 'wb') as f:
     pickle.dump(credentials, f)
 
-
-# Search files from file name
+# Get google drive service
 service = build('drive', 'v3', credentials=credentials)
-files = service.files().list(q="name contains '%s'" % args.file_name).execute()
 
-# Download file
-for f in files['files']:
-    if f['mimeType'] == 'application/vnd.google-apps.folder':
-        # Skip folder
-        continue
-    if f['mimeType'].startswith('application/vnd.google-apps'):
-        # Get Google Docs file type to pdf
-        request = service.files().export_media(fileId=f['id'], mimeType='application/pdf')
-    else:
-        # Get Other format file to original file
-        request = service.files().get(fileId=f['id'])
-    buffer = open(args.output_file, 'wb')
-    downloader = MediaIoBaseDownload(buffer, request)
-    done = False
-    while not done:
-        status, done = downloader.next_chunk()
-        print("Download %d%%." % int(status.progress() * 100))
-    buffer.close()
+if args.mode == 'upload':
+    media = MediaFileUpload(args.file_name)
+    file = service.files().create(body={'name': args.output_file}, media_body=media, fields='id').execute()
+if args.mode == 'download':
+    # Search files from file name
+    files = service.files().list(q="name contains '%s'" % args.file_name).execute()
+
+    # Download file
+    for f in files['files']:
+        if f['mimeType'] == 'application/vnd.google-apps.folder':
+            # Skip folder
+            continue
+        if f['mimeType'].startswith('application/vnd.google-apps'):
+            # Get Google Docs file type to pdf
+            request = service.files().export_media(fileId=f['id'], mimeType='application/pdf')
+        else:
+            # Get Other format file to original file
+            request = service.files().get(fileId=f['id'])
+        buffer = open(args.output_file, 'wb')
+        downloader = MediaIoBaseDownload(buffer, request)
+        done = False
+        while not done:
+            status, done = downloader.next_chunk()
+            print("Download %d%%." % int(status.progress() * 100))
+        buffer.close()
